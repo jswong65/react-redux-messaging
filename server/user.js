70adcc494164c302c696395a09d94906961919f7
jswong65 <jswong65@gmail.com>
2017-12-28 21:46:40 -0500

    handle user relevant request

---
+++ server/user.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const utils = require('utility');
+const Router = express.Router();
+const models = require('./model');
+const User = models.getModel('user'); 
+const _filter = {'passwd':0, '__v':0};
+
+Router.get('/list', (req, res) => {
+	// User.remove({}, (err, doc) => {});
+	User.find({}, (err, doc) => {
+		return res.json(doc);
+	});
+});
+Router.post('/login', (req, res) => {
+	const {username, passwd} = req.body;
+	User.findOne({username, passwd:md5Passwd(passwd)}, _filter, (err, doc) => {
+		if (!doc) {
+			return res.json({code:1, msg:'username and password do not match!'});
+		}
+		res.cookie('userid', doc._id);
+		return res.json({code:0, data:doc});
+	});
+});
+Router.post('/register', (req, res) => {
+	const {username, passwd, type} = req.body;
+	User.findOne({username:username}, (err, doc) => {
+		if (doc) {
+			return res.json({code:1, msg:'user already exists'});
+		}
+		const userModel = new User({username, type, passwd:md5Passwd(passwd)});
+		userModel.save((err, doc) => {
+			if (err) {
+				return res.json({code:1, msg:'something wrong!'});
+			}
+			const {user, type, _id} = doc;
+			res.cookie('userid', _id);
+			return res.json({code:0, data:{user, type, _id}});
+		});
+	})
+});
+Router.get('/info', (req, res) => {
+	const { userid } = req.cookies;
+	// check user has cookie or not
+	if (!userid) {
+		return res.json({code:1});
+	}
+	User.findOne({_id:userid}, _filter, (err, doc) => {
+		if (err) {
+			return res.json({code:1, msg:'something wrong!'});
+		}
+		if (doc) {
+			return res.json({code:0, data:doc});
+		}
+	});
+	
+});
+
+const md5Passwd = (passwd) => {
+	const salt = "34t321g3s5d4745%&$%hy41564%^*^(*&";
+	return utils.md5(utils.md5(passwd+salt));
+};
+
+module.exports = Router;
