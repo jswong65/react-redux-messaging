1986c5723fdd31035dada200aea56890a48427a6
jswong65 <jswong65@gmail.com>
2017-12-29 22:18:56 -0500

    add reducers for chat

---
+++ src/reducers/chat.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { USER_LIST } from '../actions/types';
+
+const initState = {
+	userList:[]
+}
+const chatReducer = (state=initState, action) => {
+	switch(action.type){
+		case USER_LIST:
+			return {...state, userList:action.payload};
+		default:
+			return state;
+	}
+
+} 
+
+export default chatReducer;
