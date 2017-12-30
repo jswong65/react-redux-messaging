6439fc3d70ecbd828059045fda532fff380decae
jswong65 <jswong65@gmail.com>
2017-12-29 22:18:05 -0500

    actions for chat

---
+++ src/actions/chat.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { USER_LIST } from './types';
+
+// add a list of user to application state
+const userList = data => ({ type:USER_LIST, payload:data });
+// obtain a list of user with the given user type (Async)
+const getUserList = type => {
+	return dispatch => {
+		axios.get(`/user/list?type=${type}`)
+		.then(res => {
+			if (res.data.code===0) {
+				dispatch(userList(res.data.data));
+			}
+		})
+	};
+};
+
+export { userList, getUserList };
