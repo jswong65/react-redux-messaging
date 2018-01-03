d307397b1724cc33f2151d68c5ca5c23c44c1e47
jswong65 <jswong65@gmail.com>
2018-01-02 22:48:06 -0500

    add ', (req, res) => {

--- server/user.js
+++ server/user.js
@@ -3,19 +3,51 @@ const utils = require('utility');
 const Router = express.Router();
 const models = require('./model');
 const User = models.getModel('user'); 
-const _filter = {'passwd':0, '__v':0};
+const Chat = models.getModel('chat');
+const _filter = { 'passwd':0, '__v':0 };
 
 Router.get('/list', (req, res) => {
-	// User.remove({}, (err, doc) => {});
 	const { type } = req.query;
 	User.find({ type }, (err, doc) => {
-		return res.json({code:0, data:doc});
+		return res.json({ code:0, data:doc });
+	});
+});
+// get msg list from a user (either sent or received)
+Router.get('/msglist', (req, res) => {
+	const userid = req.cookies.userid;
+	let users = {}
+	// retrieve user list
+	User.find({}, (err, userdoc) => {
+		userdoc.forEach( val => {
+			users[val._id] = { username:val.username, avatar:val.avatar };
+		});
+	});
+
+	Chat.find({'$or':[{ from:userid }, { to:userid }]}, (err, doc) => {
+		if (!err) {
+			return res.json({ code:0, msgs:doc, users });
+		}
+		return res.json({ code:1 });
+	});
+})
+Router.post('/readmsg', (req, res) => {
+	const userid = req.cookies.userid;
+	const { fromId } = req.body;
+	Chat.update(
+			{ from:fromId, to:userid }, 
+			{ '$set': { has_read:true } }, 
+			{ 'multi': true },
+			(err, doc) => {
+		if (!err) {
+			return res.json({ code:0, num:doc.nModified });
+		}
+		return res.json({ code:1 })
 	});
 });
 Router.post('/update', (req, res) => {
 	const userid = req.cookies.userid;
 	if (!userid) {
-		return res.json({code:1})
+		return res.json({ code:1, msg:'update failed' })
 	}
 	const body = req.body;
 	User.findByIdAndUpdate(userid, body, (err, doc) =>{
@@ -23,17 +55,17 @@ Router.post('/update', (req, res) => {
 			username:doc.username,
 			type:doc.type
 		}, body);
-		return res.json({code:0, data});
+		return res.json({ code:0, data });
 	})
 })
 Router.post('/login', (req, res) => {
 	const {username, passwd} = req.body;
 	User.findOne({username, passwd:md5Passwd(passwd)}, _filter, (err, doc) => {
 		if (!doc) {
-			return res.json({code:1, msg:'username and password do not match!'});
+			return res.json({ code:1, msg:'username and password do not match!' });
 		}
 		res.cookie('userid', doc._id);
-		return res.json({code:0, data:doc});
+		return res.json({ code:0, data:doc });
 	});
 });
 Router.post('/register', (req, res) => {
@@ -72,7 +104,7 @@ Router.get('/info', (req, res) => {
 
 const md5Passwd = (passwd) => {
 	const salt = "34t321g3s5d4745%&$%hy41564%^*^(*&";
-	return utils.md5(utils.md5(passwd+salt));
+	return utils.md5(utils.md5(passwd + salt));
 };
 
 module.exports = Router;
