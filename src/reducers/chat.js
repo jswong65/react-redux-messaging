9f2f6bf4ff0c846f4dfdae0fb774195e514829ef
jswong65 <jswong65@gmail.com>
2018-01-02 22:56:07 -0500

    create a actoion reducer for chat actions

--- src/reducers/chat.js
+++ src/reducers/chat.js
@@ -1,17 +1,34 @@
-import axios from 'axios';
-import { USER_LIST } from '../actions/types';
+import { MSG_LIST, MSG_READ, MSG_RECV } from '../actions/types';
 
 const initState = {
-	userList:[]
-}
+	chatMsg:[],
+	users:{},
+	unread:0
+};
 const chatReducer = (state=initState, action) => {
 	switch(action.type){
-		case USER_LIST:
-			return {...state, userList:action.payload};
+		case MSG_LIST:
+			return { ...state, 
+				users: action.payload.users,
+				chatMsg: action.payload.msgs, 
+				unread: action.payload.msgs.filter(val => !val.has_read && val.to === action.payload.userid).length 
+			};
+		case MSG_RECV:
+			const n = action.payload.to === action.userid ? 1 : 0
+			return { ...state, chatMsg:[...state.chatMsg, action.payload], unread:state.unread + n };
+		case MSG_READ:
+			const {fromId, num} = action.payload;
+			return  { 
+				...state, 
+				chatMsg: state.chatMsg.map(val => ({
+					...val,
+					has_read : fromId === val.from ? true : val.has_read
+				})), 
+				unread:state.unread - num 
+			};
 		default:
 			return state;
 	}
-
-} 
+};
 
 export default chatReducer;
