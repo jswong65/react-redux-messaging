84b24a7d974598d6d7e7ec087ce0b9282b6f0a5c
jswong65 <jswong65@gmail.com>
2018-01-02 22:51:37 -0500

    add action creators for receiving, send messages as well as retrieving message list

--- src/actions/chat.js
+++ src/actions/chat.js
@@ -1,18 +1,53 @@
 import axios from 'axios';
-import { USER_LIST } from './types';
+import io from 'socket.io-client';
+import { MSG_LIST, MSG_READ, MSG_RECV } from '../actions/types';
 
-// add a list of user to application state
-const userList = data => ({ type:USER_LIST, payload:data });
-// obtain a list of user with the given user type (Async)
-const getUserList = type => {
+const socket = io('ws://localhost:9093');
+
+const readMsg = fromId => {
+	return (dispatch, getState) => {
+		axios.post('/user/readmsg', {fromId})
+			.then(res => {
+				const userId = getState().user._id;
+				if (res.status === 200 && res.data.code === 0) {
+					dispatch(msgRead({ fromId, userId, num: res.data.num }))
+				}
+			})
+	}
+};
+const sendMsg = ({ from, to, msg }) => {
 	return dispatch => {
-		axios.get(`/user/list?type=${type}`)
-		.then(res => {
-			if (res.data.code===0) {
-				dispatch(userList(res.data.data));
-			}
+		socket.emit('sendMsg', {from, to, msg});
+	}
+};
+const recvMsg = () => {
+	return (dispatch, getState) => {
+		socket.on('recvmsg', data => {
+			const userid = getState().user._id;
+			dispatch(msgRecv(data, userid));
 		})
-	};
+	}
+};
+const getMsgList = () => {
+	return (dispatch, getState) => {
+		axios.get('/user/msglist')
+			.then(res => {
+				if (res.status === 200 && res.data.code === 0){
+					const userid = getState().user._id;
+					dispatch(msgList(res.data.msgs, res.data.users, userid));
+				}
+			})
+	}
 };
 
-export { userList, getUserList };
+// action creators
+const msgList = (msgs, users, userid) => { 
+	return {type:MSG_LIST, payload:{msgs, users, userid}};
+};
+const msgRecv = (msg, userid) => {
+	return { type:MSG_RECV, payload:msg, userid };
+}
+const msgRead = ({fromId, userId, num}) => {
+	return { type:MSG_READ, payload: {fromId, userId, num} };
+}
+export { getMsgList, sendMsg, recvMsg, readMsg};
